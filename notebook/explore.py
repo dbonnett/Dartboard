60d66f8b4b0e4c1a4192af870056437a826c9abd
Danny B <dbonnett1147@gmail.com>
2026-08-12 01:43:15 -0400

    adjusted combos for bullseye rule

--- notebook/explore.py
+++ notebook/explore.py
@@ -14,12 +14,21 @@ threeDartCombos = 0
 listOfCombos = []
 d = {n: 0 for n in numbers}
 
+# for hitting bullseye with first dart
+listOfCombos.append([50])
+d[50] = 1
+
 for i in numbers:
   if (50 - i) in numbers:
     twoDartCombos += 1
     listOfCombos.append([i, 50-i])
     d[i] += 1
     d[50-i] += 1
+    # for hitting bullseye second
+    twoDartCombos += 1
+    listOfCombos.append([i, 50])
+    d[i] += 1
+    d[50] += 1
   for j in numbers:
     if (50 - i - j) in numbers:
       threeDartCombos += 1
@@ -27,15 +36,19 @@ for i in numbers:
       d[i] += 1
       d[j] += 1
       d[50-i-j] += 1
-listOfCombos.append([50])
-d[50] = 1
+      # for hitting bullseye third
+      threeDartCombos += 1
+      listOfCombos.append([i, j, 50])
+      d[i] += 1
+      d[j] += 1
+      d[50] += 1
 
 print(f"Ways to win with one dart: {oneDartCombos}, two darts: {twoDartCombos}, three darts: {threeDartCombos}, \ntotal combos: {oneDartCombos + twoDartCombos + threeDartCombos}")
-print(len(listOfCombos))
+print(f"list of combos length: {len(listOfCombos)}")
 
 # find most popular numbers found in combination
 plt.bar(d.keys(), d.values())
 plt.xlabel("Score")
 plt.ylabel("Occurences")
-plt.title("Dart score occurences in all possible winning games")
+plt.title("Individual dart score occurences in all possible winning games")
 plt.show()
