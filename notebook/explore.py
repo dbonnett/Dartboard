a58992e77cb2aced42a9f3706028b4b86f300be5
Danny B <dbonnett1147@gmail.com>
2026-08-12 01:17:17 -0400

    all combos mapped/ charted

--- notebook/explore.py
+++ notebook/explore.py
@@ -1,24 +1,41 @@
 # FIND ALL POSSIBLE SCORE COMBINATIONS
+import matplotlib.pyplot as plt
 
 # start with all possible numbers you can score:
 numbers = list(range(1, 51))
 for x in [23, 29, 31, 35, 37, 41, 43, 44, 46, 47, 49]:
   numbers.remove(x)
-print(numbers)
 # there are 39 possible scores between 1-50 
 
 # count number of combinations to reach 50
-singleDartCombos = 1
-doubleDartCombos = 0
-tripleDartCombos = 0
+oneDartCombos = 1
+twoDartCombos = 0
+threeDartCombos = 0
+listOfCombos = []
+d = {n: 0 for n in numbers}
 
 for i in numbers:
   if (50 - i) in numbers:
-    doubleDartCombos += 1
-
-for i in numbers:
+    twoDartCombos += 1
+    listOfCombos.append([i, 50-i])
+    d[i] += 1
+    d[50-i] += 1
   for j in numbers:
     if (50 - i - j) in numbers:
-      tripleDartCombos += 1
+      threeDartCombos += 1
+      listOfCombos.append([i, j, 50-i-j])
+      d[i] += 1
+      d[j] += 1
+      d[50-i-j] += 1
+listOfCombos.append([50])
+d[50] = 1
 
-print(f"Ways to win with one dart: {singleDartCombos}, two darts: {doubleDartCombos}, three darts: {tripleDartCombos}, \ntotal combos: {singleDartCombos + doubleDartCombos + tripleDartCombos}")
+print(f"Ways to win with one dart: {oneDartCombos}, two darts: {twoDartCombos}, three darts: {threeDartCombos}, \ntotal combos: {oneDartCombos + twoDartCombos + threeDartCombos}")
+print(len(listOfCombos))
+
+# find most popular numbers found in combination
+plt.bar(d.keys(), d.values())
+plt.xlabel("Score")
+plt.ylabel("Occurences")
+plt.title("Dart score occurences in all possible winning games")
+plt.show()
